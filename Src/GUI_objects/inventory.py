96fd089295637fabae752e9e5d00202c26143e9e
Burymuru <matbx6@gmail.com>
2024-10-20 21:07:16 +0200

    inventářové sloty

--- Src/GUI_objects/inventory.py
+++ Src/GUI_objects/inventory.py
@@ -1,1 +1,30 @@
 from common_GUI_elements import *
+
+class Item_slot:
+
+    def update_text(self):
+        if not self.stored_item == None:
+            self.text = self.stored_item.name
+        else:
+            self.text = ""
+
+    def __init__(self):
+        self.stored_item = None
+        self.text = ""
+
+        self.update_text()
+
+class Equipement_slot(Item_slot):
+
+    def update_text(self):
+        if not self.stored_item == None:
+            self.text = self.stored_item.name
+        else:
+            self.text = self.equipement_type
+
+    def __init__(self, equipement_type):
+        self.equipement_type = equipement_type
+        self.stored_item = None
+        self.text = ""
+
+        self.update_text()
