a0a0ad30c6d680d9eb8d4b1c36c2786d730399f2
Daniel <daniel.n3007@gmail.com>
2024-10-20 19:53:15 +0200

    weapons, enemies

---
+++ Src/Entities/common_entities.py
@@ -0,0 +1,14 @@
+class Entity():
+    name = ""
+    health_lvl = 0
+    max_hp = 0
+    current_hp = 0
+    healing_lvl = 0
+    ealing_amount = 0
+    damage_lvl = 0
+    damage_per_hit = 0
+    lvl = 0
+    exp = 0
+
+
+   
--- Src/Entities/enemies.py
+++ Src/Entities/enemies.py
@@ -1,1 +1,14 @@
 from common_entities import *
+
+class Enemy(Entity):
+    ...
+
+
+
+slime_enemy = Enemy("Slime", 1, 1, 1, 1, 50)
+
+golem_enemy = Enemy("Golem", 6, 0, 2, 6, 500)
+
+wyvern_enemy = Enemy("Wyvern", 5, 6, 7, 16, 2000)
+
+dragon_enemy = Enemy("Dragon", 20, 5, 10, 35, 10000)
